8265a2b87075e48710dcbc61ed025a8978614235
Brian5m1th <briandiscbol@gmail.com>
2025-06-10 20:15:24 -0300

    feat: implementa Fluxo do repository , buscar funcionario por id , teste ok

--- src/main/java/br/com/first/gestorapi/infra/FuncionarioInfraRepository.java
+++ src/main/java/br/com/first/gestorapi/infra/FuncionarioInfraRepository.java
@@ -2,11 +2,14 @@ package br.com.first.gestorapi.infra;
 
 import br.com.first.gestorapi.application.service.FuncionarioRepository;
 import br.com.first.gestorapi.domain.Funcionario;
+import br.com.first.gestorapi.handler.APIException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 @Log4j2
@@ -25,6 +28,15 @@ public class FuncionarioInfraRepository implements FuncionarioRepository {
         List<Funcionario> funcionarioList = funcionarioSpringDataJpaRepository.findAll();
         log.info("[finish] FuncionarioInfraRepository - findByAll");
         return funcionarioList;
+    }
+
+    @Override
+    public Funcionario findById(UUID idFuncionario) {
+        log.info("[start] FuncionarioInfraRepository - findById");
+        Funcionario funcionario = funcionarioSpringDataJpaRepository.findByIdFuncionario(idFuncionario)
+                .orElseThrow(() -> APIException.build(HttpStatus.NOT_FOUND,"Cliente não encontrado"  ));
+        log.info("[finish] FuncionarioInfraRepository - findById");
+        return funcionario;
     }
 
 }
