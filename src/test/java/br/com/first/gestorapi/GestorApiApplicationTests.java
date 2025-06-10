f5e8ab1d10af97ff41499514fc452c514325918d
Brian5m1th <briandiscbol@gmail.com>
2025-06-10 17:05:29 -0300

    primeiro commit

---
+++ src/test/java/br/com/first/gestorapi/GestorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.first.gestorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
